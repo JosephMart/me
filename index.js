f393781e961201b47ac4273fce3574ac66d9bff9
Joseph Martinsen <joseph@martinsen.com>
2019-01-24 12:07:53 -0600

    Setup name to display

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const figlet = require('figlet');
+
+figlet('Joseph Martinsen', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
