8101af46c283d80c1cbdc6075ab79d01b17c84fa
Joseph Martinsen <joseph@martinsen.com>
2019-02-05 09:03:13 -0600

    Added personal links

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
-const figlet = require('figlet');
+const figlet = require("figlet");
 
-figlet('Joseph Martinsen', function (err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
+figlet("Joseph Martinsen", function(err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
 
-    console.log(data);
-    console.log('Howdy my name is Joseph Martinsen');
+  console.log(data);
+  console.log(
+    "Howdy my name is Joseph Martinsen and I am Senior Computer Engineer at Texas A&M\n"
+  );
+  console.log("Website: http://joseph.martinsen.com");
+  console.log("LinkedIn: https://linkedin.com/in/joseph-martinsen/");
+  console.log("Github: https://github.com/JosephMart");
+  console.log("Twitter: https://twitter.com/nbahoopstar05");
 });
